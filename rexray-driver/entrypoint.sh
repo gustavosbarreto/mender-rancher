9e53474d26c497ebe05617b3dc8dbfec94136bd2
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2017-06-30 15:50:15 -0300

    rexray-driver: remove peer verification logic
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

--- rexray-driver/entrypoint.sh
+++ rexray-driver/entrypoint.sh
@@ -2,14 +2,6 @@
 
 gomplate -f /etc/rexray/config.yml.tpl -o /etc/rexray/config.yml
 
-if [ "$REXRAY_SERVER_MODE" != "true" ]; then
-  mount --rbind /host/dev /dev
-
-  # Accept peer fingerprint
-  # https://rexray.readthedocs.io/en/stable/user-guide/config/#peer-verification)
-  rexray device ls <<!
-yes
-!
-fi
+mount --rbind /host/dev /dev
 
 rexray start -f
